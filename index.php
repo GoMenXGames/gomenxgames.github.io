64a09faf5af91b9c1de76864013d58ecef4a8c98
GoMenXGames <GoMenXGames@users.noreply.github.com>
2021-02-15 01:59:41 +0500

    SiteCreate

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>GoMenXGames</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <style>
+	body {background-image: url ("images/bgsakamoto.jpg");}
+  <style/>
+  <body>
+    
+  </body>
+</html>
